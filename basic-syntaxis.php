691c51cf36af6a7589d0776f7e763d151f3be7a5
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-16 06:45:53 -0500

    basic syntax

---
+++ basic-syntaxis.php
@@ -0,0 +1,17 @@
+<?php
+
+// echo "Some text";
+
+$name = "Angel";
+$last_name = "San";
+
+// echo "my name is " . $name . " " . $last_name . "\n"; 
+echo "my name is $name $last_name";  // only with double quotes
+$people = [
+    "Angel" => 50,
+    "Mario" => 15,
+];
+
+var_dump($people);
+
+print_r($people);
---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+
+$number_1 = 8;
+$number_2 = 7;
+
+define("NUMERO_PI", 3.14);
+
+echo $number_1 + $number_2;
+
+echo NUMERO_PI;
+
+echo "\n";
