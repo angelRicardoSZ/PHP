0bed80db45f598c7f499d028a5a1bb331f858186
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-24 09:33:52 -0500

    casting

---
+++ casting.php
@@ -0,0 +1,41 @@
+<?php
+
+// String to integer
+
+$number = "10";
+$number2 = (int) $number;
+
+var_dump($number);
+var_dump($number2);
+
+// float to integer 
+
+$var1 = 3.14;
+$var2 = (int) $var1;
+
+
+echo "-----------------";
+echo "\n";
+echo "float to integer";
+
+echo "\n";
+var_dump($var1);
+var_dump($var2);
+
+echo "\n";
+
+
+// number to boolean
+$var3 = 0;
+$var4 = (bool) $var3;
+
+echo "-----------------";
+echo "\n";
+echo "number to boolean";
+
+echo "\n";
+var_dump($var3);
+var_dump($var4);
+
+echo "\n";
+
